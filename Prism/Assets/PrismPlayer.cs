1a3648fee4bbf020c3834c8f9e94730c38c69f9d
stapleton <56270816+garrondenney74@users.noreply.github.com>
2022-03-07 21:32:07 -0600

    Making of the goal and the raycast

--- Prism/Assets/PrismPlayer.cs
+++ Prism/Assets/PrismPlayer.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PrismPlayer : MonoBehaviour
 {
     //VARS
+    private LineRenderer laserLine;
 
+
+
+    [Header("Set in Inspector")]
     public float jumpheight = 10f;
 
 
@@ -13,12 +17,24 @@ public class PrismPlayer : MonoBehaviour
     void Start()
     {
         Rigidbody Prism = GetComponent<Rigidbody>();
+        laserLine = GetComponent<LineRenderer>();
     }
 
+    void shoot() 
+
+    {
+        RaycastHit hit;
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(transform.position,transform.forward,out hit)) { if (hit.collider.gameObject.tag == "Tagged") { Debug.DrawRay(transform.position, transform.forward, Color.green); print("Hit"); } }
+        }
+    }
+        
     // Update is called once per frame
     // Update, check if either W a S or D is pressed
     void Update()
     {
+        //allows for the ability to control the prism using the wasd keys  w is to jump
         Rigidbody Prism = GetComponent<Rigidbody>();
         if (Input.GetKey(KeyCode.A))
             Prism.AddForce(Vector3.left);
@@ -28,6 +44,11 @@ public class PrismPlayer : MonoBehaviour
             Prism.AddForce(Vector3.down);
         if (Input.GetKey(KeyCode.W))
             Prism.AddForce(Vector3.up * jumpheight);
+
+        //checks if the player shoots out a ray that reflects
+
+
+
 
     }
 }
